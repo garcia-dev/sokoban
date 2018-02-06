e0f584f255245672e006014ce5fb34a220fbc8af
Dylan <21602573@etu.unicaen.fr>
2018-02-06 17:25:16 +0100

    Completing Score class.

--- dev/src/model/Score.java
+++ dev/src/model/Score.java
@@ -1,4 +1,43 @@
 package model;
 
 public class Score {
+
+    private String playerName;
+    private int score;
+    private int stepCount;
+    private double gameLength;
+    private int triesCount;
+
+    public Score(){
+        this.playerName = "";
+        this.score = 0;
+        this.stepCount = 0;
+        this.gameLength = 0;
+        this.triesCount = 0;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getStepCount() {
+        return stepCount;
+    }
+
+    public double getGameLength() {
+        return gameLength;
+    }
+
+    public int getTriesCount() {
+        return triesCount;
+    }
+
+    public void setPlayerName(String playerName){
+        this.playerName = playerName;
+    }
+
 }
