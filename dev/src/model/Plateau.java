970c89196f308abec71ced8ffac43e6a76337a65
Dylan <21602573@etu.unicaen.fr>
2018-03-15 15:54:01 +0100

    Adding some test images for ihm and completing the ihm

--- dev/src/model/Plateau.java
+++ dev/src/model/Plateau.java
@@ -1,11 +1,33 @@
 package model;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import view.ihm;
+
+import java.io.File;
+import java.net.URL;
+
 public class Plateau implements Observer{
 
     private Board board;
 
-    public Plateau(Board board){
+    private ihm ihm;
+
+    private int decalageX;
+
+    private int decalageY;
+
+    public Plateau(Board board, ihm ihm){
         this.board = board;
+        this.ihm = ihm;
+        this.decalageX = 20;
+        this.decalageY = 20;
+    }
+
+    public view.ihm getIhm() {
+        return ihm;
     }
 
     public Board getBoard(){
@@ -14,31 +36,50 @@ public class Plateau implements Observer{
 
     @Override
     public void update(Object source) {
+        System.out.println("oui");
+
+        Canvas canvas = new Canvas( 800, 600 );
+        this.getIhm().groupe.getChildren().add( canvas );
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        int cpt1 = 0;
+        int cpt2 = 0;
+
         for (Case[] row : this.getBoard().getLevel().getCaseArray()){
             for (Case actualCase : row){
 
                 if (actualCase.getPawn() == null){
                     switch (actualCase.getState()){
                         case TARGET:
+                            Image target = new Image(getClass().getResource("target.png").toString());
+                            gc.drawImage(target, cpt1*decalageX, cpt2*decalageY);
                             break;
                         case EMPTY:
+                            Image empty = new Image(getClass().getResource("empty.png").toString());
+                            gc.drawImage(empty, cpt1*decalageX, cpt2*decalageY);
                             break;
                         case WALL:
-                            System.out.println("WALL");
+                            Image wall = new Image(getClass().getResource("wall.png").toString());
+                            gc.drawImage(wall, cpt1*decalageX, cpt2*decalageY);
                             break;
                     }
                 }
                 else{
                     switch (actualCase.getPawn().getType()){
                         case CRATE:
-                            System.out.println("CRATE");
+                            Image box = new Image(getClass().getResource("box.png").toString());
+                            gc.drawImage(box, cpt1*decalageX, cpt2*decalageY);
                             break;
                         case PLAYER:
-                            System.out.println("PLAYER");
+                            Image player = new Image(getClass().getResource("player.png").toString());
+                            gc.drawImage(player, cpt1*decalageX, cpt2*decalageY);
                             break;
                         }
                     }
+                    cpt1 ++;
                 }
+                cpt2 ++;
+            cpt1 = 0;
             }
         }
     }
--- dev/src/view/ihm.java
+++ dev/src/view/ihm.java
@@ -21,36 +21,29 @@ import controller.GameController;
 
 public class ihm extends Application{
 
+    public Group groupe;
+
+    public Scene scene;
+
+    public ihm(){
+        this.groupe = new Group();
+        this.scene =  new Scene(groupe, 800, 600, Color.BEIGE);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Plateau plateau = new Plateau(new Board());
+        Plateau plateau = new Plateau(new Board(),this);
         plateau.getBoard().setLevel(LevelLoader.loadFile("level1",plateau.getBoard()));
+        plateau.update(this);
         GameController gameController = new GameController(plateau.getBoard());
 
         plateau.getBoard().addObserver(plateau);
 
-        Group groupe = new Group();
-        Scene scene = new Scene(groupe, 800, 600, Color.BEIGE);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Sokoban");
-        scene.setOnKeyPressed(event -> {
-                switch (event.getCode()) {
-                    case UP:
-                        gameController.move(plateau.getBoard().getLevel().getPlayerCase().getPawn(), Direction.UP);
-                        break;
-                    case DOWN:
-                        gameController.move(plateau.getBoard().getLevel().getPlayerCase().getPawn(), Direction.RIGHT);
-                        break;
-                    case LEFT:
-                        gameController.move(plateau.getBoard().getLevel().getPlayerCase().getPawn(), Direction.DOWN);
-                        break;
-                    case RIGHT:
-                        gameController.move(plateau.getBoard().getLevel().getPlayerCase().getPawn(), Direction.LEFT);
-                        break;
-                }
-            });
-            primaryStage.show();
+
+        primaryStage.show();
         }
 
 
